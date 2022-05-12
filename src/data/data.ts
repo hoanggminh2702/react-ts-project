793d498ccf8e9d4bec8ce21d4b079bc8e5c3d8e2
hoanggminh2702 <43093294+hoanggminh2702@users.noreply.github.com>
2022-05-12 10:01:55 +0700

    price reported table receive dataSource, columns

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,3 +1,4 @@
+import { ColumnType } from "antd/lib/table";
 import Images from "../assets/images/Images";
 
 export type MovieType = {
@@ -63,4 +64,121 @@ export const footerData = [
   "Careers",
   "Culture",
   "Classics",
+];
+
+// Mock price report table data
+type TablePropertyDataType = string | number;
+
+type ColumnsAlias =
+  | "option"
+  | "free"
+  | "shared"
+  | "basic"
+  | "standard"
+  | "premium"
+  | "highPerformance";
+
+type DataType = {
+  [index in ColumnsAlias]: TablePropertyDataType;
+};
+
+type MyColumnType = Omit<ColumnType<DataType>, "dataIndex" | "key"> & {
+  dataIndex?: ColumnsAlias;
+  key?: MyColumnType["dataIndex"];
+};
+
+export const dataSource: DataType[] = [
+  {
+    option: "Web, mobile, or API apps",
+    free: 10,
+    shared: 100,
+    basic: "Umlimited",
+    standard: "Umlimited",
+    premium: "Umlimited",
+    highPerformance: "Umlimited",
+  },
+  {
+    option: "Disk space",
+    free: "1 GB",
+    shared: "1 GB",
+    basic: "10 GB",
+    standard: "50 GB ",
+    premium: "25 GB",
+    highPerformance: "1 TB",
+  },
+  {
+    option: "Maximum instance",
+    free: "-",
+    shared: "-",
+    basic: "Up to 3",
+    standard: "Up to 10",
+    premium: "UP to 30*",
+    highPerformance: "Up to 100",
+  },
+
+  {
+    option: "Custom domain",
+    free: "-",
+    shared: "Supported",
+    basic: "Supported",
+    standard: "Supported",
+    premium: "Supported",
+    highPerformance: "Supported",
+  },
+  {
+    option: "Auto Scale",
+    free: "-",
+    shared: "-",
+    basic: "-",
+    standard: "Supported",
+    premium: "Supported",
+    highPerformance: "Supported",
+  },
+  {
+    option: "Hybrid Connectivity",
+    free: "-",
+    shared: "-",
+    basic: "Supported",
+    standard: "Supported",
+    premium: "Supported",
+    highPerformance: "Supported",
+  },
+];
+
+export const columns: MyColumnType[] = [
+  {
+    title: "",
+    dataIndex: "option",
+    key: "option",
+  },
+  {
+    title: "Free Try for Free",
+    dataIndex: "free",
+    key: "free",
+  },
+  {
+    title: "Shared Environment For Dev Test",
+    dataIndex: "shared",
+    key: "shared",
+  },
+  {
+    title: "Basic Dedicated environment for dev/test",
+    dataIndex: "basic",
+    key: "basic",
+  },
+  {
+    title: "Standard Run Production workloads",
+    dataIndex: "standard",
+    key: "standard",
+  },
+  {
+    title: "Premium Enhanced performance and scale",
+    dataIndex: "premium",
+    key: "premium",
+  },
+  {
+    title: "Isolated High-Performance Security and Isolation",
+    dataIndex: "highPerformance",
+    key: "highPerformance",
+  },
 ];
